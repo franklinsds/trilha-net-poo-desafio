61365d527414d8aeda251d9993d041befc1c37bb
Franklin Seixas <franklinsds@gmail.com>
2024-09-12 11:06:11 -0400

    first commit

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,22 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace DesafioPOO.Models
 {
-    public abstract class Smartphone
+     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
-        {
+           // Construtor
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {//parâmetros do tipo 
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;            
         }
+
+        //Atributos
+        public string Numero { get; set; }
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
+
+
+
+     
+        //Métodos
 
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
         }
 
-        public void ReceberLigacao()
+        public void ReceberLigacao(string v)
         {
             Console.WriteLine("Recebendo ligação...");
         }
